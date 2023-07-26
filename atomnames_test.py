a06a4bfbb4a7748e59aa8a55d5742bfdcf495a8a
LKnijff <49171383+LKnijff@users.noreply.github.com>
2023-07-26 16:05:37 +0200

    Fix capitalize element name

--- atomnames_test.py
+++ atomnames_test.py
@@ -1,19 +1,19 @@
 from ase.data import atomic_numbers, atomic_names
 import re
 
-atom_names = ['C', 'C1', 'Ca', 'Cablue', 'Cb', 'CH', 'C_b', 'C_alpha']
+atom_names = ['C', 'C1', 'Ca', 'CA','Cablue', 'Cb', 'CH', 'C_b', 'C_alpha']
 
 def generate_elem_num(name):
     potential_atom = re.split('[^A-Za-z]',name)[0]
 
     try:
-        atomic_numbers[potential_atom[:2]]
+        atomic_numbers[potential_atom[:2].capitalize()]
 
     except:
         element = atomic_numbers[potential_atom[0]]
 
     else:
-        element = atomic_numbers[potential_atom[:2]]
+        element = atomic_numbers[potential_atom[:2].capitalize()]
 
     return element
     
